115507c480e5e5f5d1373ffa3cad5379403ccf8e
Dennis Grabowski <dennisgrabowski@gmx.de>
2018-07-15 16:08:39 +0200

    hooks: run pre-commit tests hook when source changed

--- hooks/scripts/run-tests.sh
+++ hooks/scripts/run-tests.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env bash
 
+files_changed=$(git diff --cached --name-only)
+if [[ $(echo $files_changed | grep -svE "\.(cpp|h)$") ]]
+then
+    echo "No source file has been changed. No need to run tests."
+    exit 0
+fi
+
 repo_root=$(git rev-parse --show-toplevel)
 binary_dir=$repo_root/bin
 test_executable=$binary_dir/liblettercase-test
